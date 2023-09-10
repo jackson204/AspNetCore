9dc253d5a096c07d5695d06b8e5d3bbad7847159
jackson <s97403002a@gmail.com>
2023-09-10 20:58:49 +0800

    異部方法async await

---
+++ AsyncAwaitDemo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Before writing to file");
+            var filename = @"C:\Users\s9740\RiderProjects\AspNetCore\AsyncAwaitDemo1\test.txt";
+            var sb = new StringBuilder();
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.AppendLine($"Hello {i}");
+            }
+            await File.WriteAllTextAsync(filename, sb.ToString());
+            Console.WriteLine("Before reading from file");
+            var result = await File.ReadAllTextAsync(filename);
+            Console.WriteLine(result);
+        }
+    }
+}
