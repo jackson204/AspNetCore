aa182a75d4fbe53546035747a36bb8cb33f280be
jackson <s97403002a@gmail.com>
2023-09-18 23:16:52 +0800

    delegate

---
+++ DelegateDemo1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegateDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Method1;
+            myDelegate();
+            MyDelegate2 myDelegate2 = Add;
+            var result = myDelegate2(1,3);
+            Console.WriteLine(result);
+        }
+        static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    
+    delegate void MyDelegate();
+    
+    delegate int MyDelegate2(int a, int b);
+}
